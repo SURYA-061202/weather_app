cdc81bf60c375d423781ed2cba01e9b6740ea903
SURYA-061202 <132092219+SURYA-061202@users.noreply.github.com>
2023-10-04 12:07:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import WeatherApp from './Components/WeatherApp/WeatherApp.jsx';
+import ReactDOM from 'react-dom/client'
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <WeatherApp/>
+      </div>
+    
+  );
+}
+
+export default App;
